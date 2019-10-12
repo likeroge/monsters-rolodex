7dab81ebf6ec35dcbe66f63800559794ec38e1ae
Egor <likeroge@gmail.com>
2019-10-12 19:13:43 +0400

    Updates

---
+++ precache-manifest.ae48a2d5a417c1a2edd1ad9f0284f008.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bcd09ebaa4271b24e22ec928b4c4d76",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "da75083cd40455af734f",
+    "url": "/monsters-rolodex/static/css/main.5091a80c.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "da75083cd40455af734f",
+    "url": "/monsters-rolodex/static/js/main.2c5800d6.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
